d158cc384480221a1b30e8cb30156d39dea0fb1c
shanti867 <shantikumari9311@gmail.com>
2026-08-02 21:13:31 +0530

    we have updated today's code

--- src/main/java/com/example/Shopmart_Backend1/Service/ProductServiceImpl.java
+++ src/main/java/com/example/Shopmart_Backend1/Service/ProductServiceImpl.java
@@ -26,6 +26,17 @@ public class ProductServiceImpl implements ProductService{
     private BrandRepository brandRepo;
 
     public Product save(ProductRequest request)throws Exception{
+        System.out.println("========== PRODUCT DATA ==========");
+
+        System.out.println("Name : " + request.getName());
+
+        System.out.println("MainCategory ID : " + request.getMainCategoryId());
+
+        System.out.println("SubCategory ID : " + request.getSubCategoryId());
+
+        System.out.println("Brand ID : " + request.getBrandId());
+
+        System.out.println("==================================");
         Product product = new Product();
         product.setName(request.getName());
         product.setMaincategory(
@@ -45,15 +56,28 @@ public class ProductServiceImpl implements ProductService{
         product.setStock(request.getStock());
         product.setStockQuantity(request.getStockQuantity());
         product.setDescription(request.getDescription());
+
         List<String> images = new ArrayList<>();
+        String uploadDir = System.getProperty("user.dir")
+                + File.separator
+                + "uploads"
+                + File.separator
+                + "product";
+        File directory = new File(uploadDir);
+        if(!directory.exists()){
+            directory.mkdirs();
+        }
         for(MultipartFile file: request.getPic()){
             String fileName = System.currentTimeMillis()+"_"+file.getOriginalFilename();
-            String path = "uploads/product"+fileName;
-            file.transferTo(new File(path));
+            File destination = new File(directory, fileName);
+            System.out.println("UPLOAD PATH : " + destination.getAbsolutePath());
+            System.out.println("FOLDER EXISTS : " + directory.exists());
+            file.transferTo(destination);
             images.add(fileName);
         }
         product.setPic(images);
         product.setStatus(request.getStatus());
+
         Product savedProduct = productRepo.save(product);
         savedProduct.setProductId(
                 "PRD" + String.format("%03d", savedProduct.getId())
